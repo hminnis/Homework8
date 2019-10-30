9dd084848ff72e00fc932ccd237862de0a523f84
hminnis <56447323+hminnis@users.noreply.github.com>
2019-10-30 11:51:40 -0400

    Data entered. Plot is reactive.

--- app.R
+++ app.R
@@ -1,47 +1,63 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#######
+## Homework 8
+#######
+
+
+
+#load in needed packages
 
 library(shiny)
+library(tidyverse)
+
+
+
+#set slider range to the range of color in dataset
+min.depth <- min(diamonds$depth)
+max.depth <- max(diamonds$depth)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Diamonds Data Set Viewer"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         sliderInput("depth.adjuster",
+                     "Depth",
+                     min = min.depth,
+                     max = max.depth,
+                     value = c(min.depth, max.depth)),
+         submitButton("Go!")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("diamonds_depth_plot")
       )
    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  depth_filt <- reactive({
+    
+    
+    low.depth <- input$depth.adjuster[1]
+    hi.depth <- input$depth.adjuster[2]
+    
+    depth_filt <- diamonds %>%
+      filter(color >= low.depth) %>%
+      filter(color <= hi.depth)
+  })
+  
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+   output$diamonds_depth_plot <- renderPlot({
+     ggplot(depth_filt(), aes_string(x = "depth", y = "carat", color = "clarity")) +
+       geom_point()
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
 }
 
